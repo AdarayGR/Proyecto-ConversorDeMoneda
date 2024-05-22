5b3c399e9aafed843e2b60d56b82680e37d06d86
AdarayGR <alonso.jaagr@gmail.com>
2024-05-22 16:21:04 -0600

    first commit

--- ConversorDeMoneda/src/Principal.java
+++ ConversorDeMoneda/src/Principal.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
+import java.util.Objects;
 
 public class Principal {
     public static void main(String[] args) {
         Object[] opciones = {"Convertir Divisas", "Salir"};
         //Cargando la imagen de mi icono:
-        ImageIcon icono = new ImageIcon(Principal.class.getResource("resources/iconConverter.png"));
+        ImageIcon icono = new ImageIcon(Principal.class.getResource("ConversorDeMoneda/resources/iconConverter.png"));
         int elige = JOptionPane.showOptionDialog(null,"¿Qué quieres hacer?", "Conversor de moneda", JOptionPane.DEFAULT_OPTION,JOptionPane.PLAIN_MESSAGE, icono, opciones, opciones[0]);
     }
 }
