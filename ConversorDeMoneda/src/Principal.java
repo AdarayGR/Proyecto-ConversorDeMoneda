578875b0b91b263bf0f27da732b696c8be503ab8
AdarayGR <alonso.jaagr@gmail.com>
2024-05-22 16:01:36 -0600

    Agregar carpeta resources al proyecto

--- ConversorDeMoneda/src/Principal.java
+++ ConversorDeMoneda/src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
     public static void main(String[] args) {
         Object[] opciones = {"Convertir Divisas", "Salir"};
         //Cargando la imagen de mi icono:
-        ImageIcon icono = new ImageIcon(Principal.class.getResource(""));
-        int elige = JOptionPane.showOptionDialog(null,"¿Qué quieres hacer?", "Conversor de moneda", JOptionPane.DEFAULT_OPTION,JOptionPane.PLAIN_MESSAGE, null, opciones, opciones[0]);
+        ImageIcon icono = new ImageIcon(Principal.class.getResource("resources/iconConverter.png"));
+        int elige = JOptionPane.showOptionDialog(null,"¿Qué quieres hacer?", "Conversor de moneda", JOptionPane.DEFAULT_OPTION,JOptionPane.PLAIN_MESSAGE, icono, opciones, opciones[0]);
     }
 }
